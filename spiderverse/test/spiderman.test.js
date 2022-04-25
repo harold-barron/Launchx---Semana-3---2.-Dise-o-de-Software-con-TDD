5c3f047f0fcee34c85f12e781283e0fb0118bbdf
Harold Barron <haroldbarron13@gmail.com>
2022-04-25 18:29:00 -0500

    Creating the class spiderman and declaring his constructor

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -2,7 +2,7 @@ describe("Unit test for spiderman class", () => {
     test('1)Create an spiderman object', () => {
       //Escribimos el código que queremos implementar
       //Objetivo : instanciar un objeto Spiderman con la siguiente info
-
+      const spiderman = require('./../app/sipiderman')
       const andrewGarfield = new spiderman("Spiderman sony", 31, "Andrew Garfield",2,"Sony")
 
       //Validamos que el código funcione de la forma adecuada 
