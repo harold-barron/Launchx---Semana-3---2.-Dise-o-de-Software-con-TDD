aef3e7e39375c9d32b1856312bf2e093e7497a9c
Harold Barron <haroldbarron13@gmail.com>
2022-04-25 18:21:44 -0500

    Writing a text for the next bit of funtionality

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit test for spiderman class", () => {
+    test('1)Create an spiderman object', () => {
+      //Escribimos el código que queremos implementar
+      //Objetivo : instanciar un objeto Spiderman con la siguiente info
+
+      const andrewGarfield = new spiderman("Spiderman sony", 31, "Andrew Garfield",2,"Sony")
+
+      //Validamos que el código funcione de la forma adecuada 
+
+      expect(andrewGarfield.name).toBe("Spiderman sony")
+      expect(andrewGarfield.age).toBe(31)
+      expect(andrewGarfield.actor).toBe("Andrew Garfield")
+      expect(andrewGarfield.movies).toBe(2)
+      expect(andrewGarfield.studio).toBe("Sony")
     });
   })
